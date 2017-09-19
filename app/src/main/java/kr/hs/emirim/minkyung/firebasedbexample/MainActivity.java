e72832d0a9c931784e569325a88867a2096279f2
DonnaKin <mkkim916@naver.com>
2017-09-19 20:28:52 +0900

    #4 피카소와 만남

--- app/src/main/java/kr/hs/emirim/minkyung/firebasedbexample/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/minkyung/firebasedbexample/MainActivity.java
@@ -1,9 +1,11 @@
 package kr.hs.emirim.minkyung.firebasedbexample;
 
+import com.squareup.picasso.Picasso;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +20,13 @@ public class MainActivity extends AppCompatActivity {
         mNameTextView = (TextView)findViewById(R.id.profile_name);
         mGithubTextView = (TextView) findViewById(R.id.profile_github);
         mProfileImageView = (ImageView)findViewById(R.id.profile_image);
+
+        Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(mProfileImageView);
+
+        Picasso.with(this)
+                .load("httpL//i.imgur.com/DvpvklR.png");
+                .centerCrop();
+                .resize(100,100);
+                .into(mProfileImageView);
     }
 }
